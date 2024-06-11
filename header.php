70038a8ec3518c9a5e61519dbd933a717bdc53ca
BuildTools <unconfigured@null.spigotmc.org>
2024-06-11 20:56:38 +0200

    ajout de jquery-3.7.1.min.js

--- header.php
+++ header.php
@@ -15,6 +15,7 @@
         <link rel="stylesheet" href="/assets/css/fonts/getAll.css">
 
         <script src="/assets/js/main.js"></script>
+        <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
 
         <script>
             <?php
