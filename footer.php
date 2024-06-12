21ef52f2a02d6a38fca76a69449423c8e5fe9711
GeOzz <163060944+GeOzz@users.noreply.github.com>
2024-06-12 08:18:04 +0200

    Add files via upload

--- footer.php
+++ footer.php
@@ -1,12 +1,25 @@
-                <footer>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt consequuntur amet culpa cum itaque neque.</p>
-                    <a href="">À propos</a>
-                    <a href="">Mentions légales</a>
-                    <a href="">Conditions d'utilisation</a>
-                    <a href="">Cookies</a>
-                    <a href="">Haut de page</a>
-                    <p>Copyright © 2024. Tout droit réservé.</p>
-                </footer>
+<footer>
+        <div class="footerContainer">
+            <img src="img/Eco3.png" alt="Seed Logo" class="footer-logo">
+            <div class="socialIcons">
+                <a href="https://www.linkedin.com" target="_blank"><i class="fab fa-linkedin-in"></i></a>
+                <a href="https://www.tiktok.com" target="_blank"><i class="fab fa-tiktok"></i></a>
+                <a href="https://www.instagram.com" target="_blank"><i class="fab fa-instagram"></i></a>
+                <a href="https://www.facebook.com" target="_blank"><i class="fab fa-facebook-f"></i></a>
+            </div>
+            <div class="footerNav">
+                <ul>
+                    <li><a href="index.html">À Propos</a></li>
+                    <li><a href="about.html">Mentions Légales</a></li>
+                    <li><a href="services.html">Conditions Générales</a></li>
+                </ul>
+                <a href="contact.html" class="contact-button">Contact</a>
+            </div>
+            <div class="footerBottom">
+                <p>&copy; 2024 Seed. Tous droits réservés.</p>
+            </div>
+        </div>
+    </footer>
             </div>
         </body>
     </html>
