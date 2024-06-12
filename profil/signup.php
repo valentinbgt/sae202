a7e51b7ae27194882ec21dbc5664127a382ab91d
BuildTools <unconfigured@null.spigotmc.org>
2024-06-12 16:17:19 +0200

    front du formulaire d'inscription

--- profil/signup.php
+++ profil/signup.php
@@ -16,19 +16,60 @@
             <div class="topHeroImage">
                 <h1>Inscription</h1>
             </div>
-            <form action="/profil/inc/signup.proc.php" method="post" enctype="multipart/form-data">
-                <input type="text" name="userName" placeholder="Nom d'utilisateur"><br>
-                <input type="text" name="userDisplayName" placeholder="Nom d'affichage"><br>
-                <input type="email" name="userEmail" placeholder="Adresse mail"><br>
-                <input type="email" name="userEmailConfirm" placeholder=" Confirmation adresse mail"><br>
-                <input type="password" name="userPassword" placeholder="Mot de passe"><br>
-                <input type="password" name="userPasswordConfirm" placeholder="Confirmer mot de passe"><br>
-                <input type="file" name="userProfilePicture"><br>
-                <input type="submit" value="S'inscrire">
+            <script src="/assets/js/signForm.js" defer></script>
+            <form id="signForm" action="/profil/inc/signup.proc.php" method="post" enctype="multipart/form-data">
+                <div class="formDouble">
+                    <div class="formBox">
+                        <label for="userDisplayName">Nom d'affichage</label>
+                        <input type="text" name="userDisplayName" id="userDisplayName" placeholder="Emma MARTIN">
+                    </div>
+
+                    <div class="formBox">
+                        <label for="userName">Nom d'utilisateur <span class="thin">(identifiant)</span></label>
+                        <input type="text" name="userName" id="userName" placeholder="emma.martin">
+                    </div>
+                </div>
+                
+                <div class="formDouble">
+                    <div class="formBox">
+                        <label for="userEmail">Adresse Mail</label>
+                        <input type="email" name="userEmail" id="userEmail" placeholder="emma.martin@gmail.com">
+                    </div>
+
+                    <div class="formBox">
+                        <label for="userEmailConfirm">Confirmer l'adresse mail</label>
+                        <input type="email" name="userEmailConfirm" id="userEmailConfirm" placeholder="emma.martin@gmail.com">
+                    </div>
+                </div>
+
+                <div class="formDouble">
+                    <div class="formBox">
+                        <label for="userPassword">Mot de passe</label>
+                        <input type="password" name="userPassword" id="userPassword" placeholder="************">
+                    </div>
+
+                    <div class="formBox">
+                        <label for="userPasswordConfirm">Confirmer le mot de passe</label>
+                        <input type="password" name="userPasswordConfirm" id="userPasswordConfirm" placeholder="************">
+                    </div>
+                </div>
+
+                <div class="formDouble">
+                    <div class="formBox">
+                        <label for="userProfilePicture">Photo de profil</label>
+                        <input hidden type="file" name="userProfilePicture" id="userProfilePicture">
+                        <img hidden id="picturePreview" alt="Prévisualisation de la photo de profil">
+                        <p class="addPicture" id="addPicture">Ajouter une photo</p>
+                    </div>
+                    <div class="formBox">
+                        <input type="submit" value="S'inscrire">
+                    </div>
+                </div>
+                
+                <div class="signup-link">
+                    <p>Vous avez déjà un compte ? <a href="/profil/signin.php?from=<?= $from; ?>">Se Connecter</a></p>
+                </div>
             </form>
-            <div class="signup-link">
-            <p>Vous avez déjà un compte ? <a href="/profil/signin.php?from=<?= $from; ?>">Se Connecter</a></p>
-            </div>
         </main>
 <?php
     require_once(DOCUMENT_ROOT . 'footer.php');
