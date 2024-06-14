95e098623cdce5af9e9960c559227d9289cef2ba
BuildTools <unconfigured@null.spigotmc.org>
2024-06-14 09:16:54 +0200

    git ignore

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
                 <h1>Seed</h1>
             </div>
             <h2 class="pageSeconTitle">Pour un meilleur avenir, <span class="more-sugar">Seed</span>.</h2>
+            <h2>Bonjour, <?= $_SESSION["user_displayName"] ?>.</h2>
         </main>
 <?php
     require_once(DOCUMENT_ROOT . 'footer.php');
