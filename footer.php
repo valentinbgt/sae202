f1bbe101512e045eddc4a0e993d0179014d8b114
BuildTools <unconfigured@null.spigotmc.org>
2024-06-14 15:02:54 +0200

    formulaire contact avancé + fix cypber index proc et inc

--- footer.php
+++ footer.php
@@ -2,15 +2,15 @@
                 <div class="footerContainer">
                     <img src="/assets/img/logo_seed.webp" alt="Seed Logo" class="footer-logo" />
                     <div class="socialIcons">
-                        <a title ="Site d'Agence" href="https://mmi23b04.sae202.ovh/agence/" target="_blank"><i class="fa fa-laptop"></i></a>
-                        <a title="Facebook" href="https://www.facebook.com" target="_blank"><i class="fab fa-facebook"></i></a>
-                        <a title="Instagram" href="https://www.instagram.com" target="_blank"><i class="fab fa-instagram"></i></a>
+                        <a title="Site de MMI Agency" href="https://mmi23b04.sae202.ovh/agence/" target="_blank"><i class="fa fa-laptop"></i></a>
+                        <a title="Seed sur Facebook" href="https://www.facebook.com" target="_blank"><i class="fab fa-facebook"></i></a>
+                        <a title="Seed sur Instagram" href="https://www.instagram.com" target="_blank"><i class="fab fa-instagram"></i></a>
                     </div>
                     <div class="footerNav">
                         <ul>
-                            <li><a title="À Propos" href="index.html">À Propos</a></li>
-                            <li><a title ="Mentions Légales" href="about.html">Mentions Légales</a></li>
-                            <li><a title="Conditions Générales" href="services.html">Conditions Générales</a></li>
+                            <li><a title="À Propos" href="#">À Propos</a></li>
+                            <li><a title ="Mentions Légales" href="#">Mentions Légales</a></li>
+                            <li><a title="Conditions Générales" href="#">Conditions Générales</a></li>
                         </ul>
                         <a title="Contact" href="/contact" class="contact-button">Contact</a>
                     </div>
